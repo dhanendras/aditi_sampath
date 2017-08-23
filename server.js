689158c823482c2f3443b26ef3a07ad7e07aa62d
unknown <U41461@E216LTRV.ustr.com>
2017-08-24 01:46:25 +0530

    k

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
-var restify = require('restify');
-var port = process.env.PORT || 8080;
+var http = require('http');
 
-function respond(req, res, next) {
-  res.send('hello ' + req.params.name);
-  next();
-}
-
-var server = restify.createServer();
-server.get('/hello/:name', respond);
-server.head('/hello/:name', respond);
-
-server.listen(port, function() {
-  console.log('%s listening at %s', server.name, server.url);
-});
+http.createServer(function (req, res) {
+    
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('Hello, world!');
+    
+}).listen(process.env.PORT || 8080);
