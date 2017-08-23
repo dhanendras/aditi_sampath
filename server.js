39ef82d4ecdd5c2d77b7299775314c3ed655ba99
unknown <U41461@E216LTRV.ustr.com>
2017-08-24 00:24:37 +0530

    lk

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var server = restify.createServer();
 server.listen(8080, function() {
     console.log('%s listening to %s', server.name, server.url);
 });
-
+/*
 
 server.get('/', function(req, res, next) {
     return 'hello');
@@ -19,7 +19,7 @@ var connector = new builder.ChatConnector({
     appPassword: '3rqVr45QvS1ONmvGi5YV1Mp'
 });
 
-
+*/
 //luis model url
 
 const LuisModelUrl = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/00075350-0d1e-4f2d-8f6e-75e253066b43?subscription-key=468963da9804413788459981febe3bb6&timezoneOffset=0&verbose=true&q= ';
