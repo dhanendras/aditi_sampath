01b217622d5b006f49bc2336728701e811270906
dhanendras <dhanendra.chaudhary@gmail.com>
2017-08-24 02:17:27 +0530

    c
    c

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-var http = require('http');
 
-http.createServer(function (req, res) {
-    
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
-    
-}).listen(process.env.PORT || 8080);
+var builder = require('botbuilder');
+var restify = require('restify');
+var analyticsService = require('./models/text-analytics');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(3978, function() {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create bot
+var connector = new builder.ChatConnector({
+    appId: '6c3e1d15-2432-402b-86b2-4b6b8f5b25a1',
+    appPassword: 'iKXaa1a6Tap6Un6XBLjFk6i'
+});
+server.post('/api/messages', connector.listen());
+
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send('Sorry, I did not understand \'%s\'. Type \'help\' if you need assistance.', session.message.text);
+});
