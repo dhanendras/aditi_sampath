1822f73227bb8dc366faffc4109f7b2e562579ea
dhanendras <dhanendra.chaudhary@gmail.com>
2017-08-24 11:20:15 +0530

    yo
    yo

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ server.listen(port, function() {
 });
 
 // Create bot
-/*
+
 var connector = new builder.ChatConnector({
     appId: '6c3e1d15-2432-402b-86b2-4b6b8f5b25a1',
     appPassword: 'iKXaa1a6Tap6Un6XBLjFk6i'
@@ -29,4 +29,4 @@ server.post('/api/messages', connector.listen());
 
 var bot = new builder.UniversalBot(connector, function (session) {
     session.send('Sorry, I did not understand \'%s\'. Type \'help\' if you need assistance.', session.message.text);
-});*/
+});
