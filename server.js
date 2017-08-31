72dafb07612dd42ee480c08636a7e151546c3e03
highflyingbird-ust <c58720@ust-global.com>
2017-08-31 12:04:06 +0530

    guidelines

--- server.js
+++ server.js
@@ -620,15 +620,15 @@ bot.dialog('feedback',[
             if (!isNaN(newScore)) {
                     if (newScore > 0.8) {
                         session.send('Thanks %s! It means a lot to us. Hope we get to see you again',session.userData.name);
-                        session.ebeginDialog('end');
+                        session.beginDialog('end');
                     } 
                     else if (newScore > 0.5) {
                         session.send('Thanks for that positive input %s. We hope to impress you next time',session.userData.name);
-                        session.ebeginDialog('end');
+                        session.beginDialog('end');
                     } 
                     else {
                         session.send('Thank you for the honest input %s. We will definetly work on it',session.userData.name);
-                        session.ebeginDialog('end');
+                        session.beginDialog('end');
                     }
 
                 }
