86faf2eaf35ebf4446e77a90d25a3e71516289f3
highflyingbird-ust <c58720@ust-global.com>
2017-10-18 18:28:34 +0530

    final for thu

--- dialogs/confirm.js
+++ dialogs/confirm.js
@@ -2,10 +2,11 @@ var builder = require('botbuilder');
 
 module.exports = [
     function(session){
-        session.delay(3000);
-        session.send('%s, would be your primary POC through this tour.',session.userData.poc);
+        session.delay(1000);
+        session.send('%s would be your primary POC through this tour.',session.userData.poc);
         session.delay(3000);
         session.send('I see that you have the %s mode set up for this tour',session.userData.demotype);
+        session.delay(2500);
         builder.Prompts.choice(session,'Would you like to change it?',"Quick|Detailed", { listStyle: 3 });
     },
     function(session,results,next){
